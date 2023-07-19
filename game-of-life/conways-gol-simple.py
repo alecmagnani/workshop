a0563be4f82bee18fdea1339c4967fbcf3aa5e81
alecmagnani <alecmagnani@gmail.com>
2023-07-18 17:48:53 -0700

    gol: more colormaps for matplotlib

--- game-of-life/conways-gol-simple.py
+++ game-of-life/conways-gol-simple.py
@@ -3,9 +3,25 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
+from functools import partial
+
 interpolation_methods = [None, 'none', 'nearest', 'bilinear', 'bicubic', 'spline16', 
                          'spline36', 'hanning', 'hamming', 'hermite', 'kaiser', 'quadric', 
                          'catrom', 'gaussian', 'bessel', 'mitchell', 'sinc', 'lanczos']
+
+color_maps = [
+    # Perceptually Uniform Sequential
+    'viridis', 'plasma', 'inferno', 'magma', 'cividis',
+    # Sequential2
+    'gray', 'bone', 'pink', 'spring', 'summer', 'autumn', 'winter', 
+    'cool', 'Wistia',  'hot', 'afmhot', 'gist_heat', 'copper',
+    # Qualitative
+    'Pastel1', 'Pastel2', 'Paired', 'Accent', 'Dark2', 'Set1', 'Set2', 'Set3', 
+    'tab10', 'tab20', 'tab20b', 'tab20c',
+    # Misc
+    'flag', 'prism', 'ocean', 'terrain', 'gnuplot', 'gnuplot2',
+    'brg', 'gist_rainbow', 'rainbow', 'jet', 'turbo'
+]
 
 ON = 255
 OFF = 0
@@ -42,6 +58,7 @@ def main():
     parser.add_argument('--grid-size', dest='N', required=False)
     parser.add_argument('--interval', dest='interval', required=False)
     parser.add_argument('--method', dest='method', required=False)
+    parser.add_argument('--cmap', dest='cmap', required=False)
     args = parser.parse_args()
 
     # Set grid size
@@ -54,21 +71,33 @@ def main():
     if args.interval:
         updateInterval = int(args.interval)
 
+    # Set interpolation method
     method = interpolation_methods[2]
     if args.method:
         if (args.method in interpolation_methods):
             method = args.method
-        elif (args.method.casefold() == 'random'.casefold()) or (args.method.casefold() == 'rand'.casefold()):
-            method = interpolation_methods[np.random.randint(0, len(interpolation_methods) - 1)]
+        elif (args.method.casefold() == 'random') or (args.method.casefold() == 'rand'):
+            method = np.random.choice(interpolation_methods, 1)[0]
+            print('Using interpolation method: ' + method)
+
+    # Set color map
+    cmap = color_maps[0]
+    if args.cmap:
+        if (args.cmap in color_maps):
+            cmap = args.cmap
+        elif (args.cmap.casefold() == 'random') or (args.cmap.casefold() == 'rand'):
+            cmap = np.random.choice(color_maps, 1)[0]
+            print('Using color map: ' + cmap)
 
     # Init grid
     grid = randomGrid(N)
 
     # Start animation
     fig, ax = plt.subplots()
-    img = ax.imshow(grid, interpolation=method)
-    ani = animation.FuncAnimation(fig, update, fargs=(img, grid, N, ),
-                                  frames=10,
+    img = ax.imshow(grid, cmap=cmap, interpolation=method)
+    ani = animation.FuncAnimation(fig, 
+                                  partial(update, img=img, grid=grid, N=N),
+                                  save_count=50,
                                   interval=updateInterval)
 
     plt.show()
